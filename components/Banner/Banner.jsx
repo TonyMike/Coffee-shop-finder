647acc4812d9257d5791042e030f001a6958820b
TonyMike <tee.jhay1@gmail.com>
2022-09-15 11:07:41 +0100

    coffee connoiseur

---
+++ components/Banner/Banner.jsx
@@ -0,0 +1,18 @@
+import styles from './banner.module.css'
+const Banner = ({ buttonText, handleonClick }) => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <span className={styles.title1}>Coffee</span>
+        <span className={styles.title2}>Connoiseur</span>
+      </h1>
+      <p className={styles.subTitle}>Discover your local coffe shop!</p>
+      <div className={styles.buttonWrapper}>
+        <button className={styles.button} onClick={handleonClick}>
+          {buttonText}
+        </button>
+      </div>
+    </div>
+  )
+}
+export default Banner
---
+++ pages/coffee-store/[store].jsx
@@ -0,0 +1,9 @@
+const Store= () => {
+  return (
+    <div>
+      Enter
+    </div>
+  );
+}
+
+export default Store;
